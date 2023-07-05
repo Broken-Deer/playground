4e7aa981b1f1110276146542ab636c3b223abed3
Broken Deer <kxscluabcbook@outlook.com>
2023-07-05 13:33:46 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,13 @@
-use mgl_core::{
-    core::{folder::MinecraftLocation, task::EventListeners},
-    installer::install,
-};
-
+use regex::Regex;
+use tokio::process::Command;
 #[tokio::main]
 async fn main() {
-    install(
-        "1.19.4",
-        MinecraftLocation::new("test"),
-        EventListeners::new().on_progress(Box::new(|a, b, c| {
-            println!("progress: {a}/{b}  step: {c}");
-        })),
-    )
-    .await;
+    let  mut command = Command::new("powershell");
+    command.args(&["-Command", "chcp 65001 && ver"]);
+    let output = command.output().await.unwrap();
+    let stdout = String::from_utf8(output.stdout).unwrap();
+    let regex = Regex::new(r"\S*.\]").unwrap();
+    let mut output = regex.find(&stdout).unwrap().as_str().to_string();
+    output.pop();
+    println!("{}", output);
 }
