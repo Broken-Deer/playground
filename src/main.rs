0091f7a2b3ab47ad2c38b6b7e3b53f8a6fb395fe
Broken Deer <kxscluabcbook@outlook.com>
2023-07-05 14:20:43 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use tokio::process::Command;
 #[tokio::main]
 async fn main() {
     let  mut command = Command::new("C:\\Windows\\System32\\cmd.exe");
-    command.args(&["/C", "chcp 65001 && ver"]);
+    command.args(&["/C", "ver"]);
     let output = command.output().await.unwrap();
     let stdout = String::from_utf8(output.stdout).unwrap();
     println!("{}", &stdout);
