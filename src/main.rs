576b97f3a3935b7097dd7e41d7c09ab548cb0c39
Broken Deer <kxscluabcbook@outlook.com>
2023-07-05 14:24:54 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,10 @@ use regex::Regex;
 use tokio::process::Command;
 #[tokio::main]
 async fn main() {
-    let mut command = Command::new("C:\\Windows\\System32\\cmd.exe");
-    command.args(&["/C", "ver"]);
+    let  mut command = Command::new("C:\\Windows\\System32\\cmd.exe");
+    command.args(&["/C", "chcp 65001 ; ver"]);
     let output = command.output().await.unwrap();
-    let stdout = unsafe { String::from_utf8_unchecked(output.stdout) };
+    let stdout = String::from_utf8(output.stdout).unwrap();
     println!("{}", &stdout);
     let regex = Regex::new(r"\S*.\]").unwrap();
     let mut output = regex.find(&stdout).unwrap().as_str().to_string();
