470e3a8e59ff328204f1bdc3e401b880289ed060
Broken Deer <kxscluabcbook@outlook.com>
2023-07-05 13:49:29 +0800

    123

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ async fn main() {
     command.args(&["-Command", "chcp 65001 && ver"]);
     let output = command.output().await.unwrap();
     let stdout = String::from_utf8(output.stdout).unwrap();
+    println!("{}", &stdout);
     let regex = Regex::new(r"\S*.\]").unwrap();
     let mut output = regex.find(&stdout).unwrap().as_str().to_string();
     output.pop();
