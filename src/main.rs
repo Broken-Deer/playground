f1e1390a3f12b68b57a2a5a0c9252b9a5fd40a3b
Broken Deer <kxscluabcbook@outlook.com>
2023-07-05 15:36:10 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
+use regex::Regex;
 // use regex::Regex;
 use tokio::process::Command;
 #[tokio::main]
 async fn main() {
     let  mut command = Command::new("C:\\Windows\\System32\\cmd.exe");
-    command.args(&["/C", "powershell -c Get-ComputerInfo | select WindowsProductName, WindowsVersion, OsHardwareAbstractionLayer"]);
+    command.args(&["/C", r#"powershell -c "Get-ComputerInfo | select OsHardwareAbstractionLayer""#]);
     let output = command.output().await.unwrap();
     let stdout = String::from_utf8(output.stdout).unwrap();
     let stderr = String::from_utf8(output.stderr).unwrap();
     println!("{}", &stdout);
     println!("{}", &stderr);
-    // let regex = Regex::new(r"\S*.\]").unwrap();
-    // let mut output = regex.find(&stdout).unwrap().as_str().to_string();
-    // output.pop();
+    // let a = r"
+    // OsHardwareAbstractionLayer
+    // --------------------------
+    // 10.0.19041.2251
+    
+    // Microsoft Windows [version 10.0]";
+    // let regex = Regex::new(r"[0-9].*\S").unwrap();
+    // let output = regex.find(&a).unwrap().as_str().to_string();
+    
     // println!("{}", output);
 }
