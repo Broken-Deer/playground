cef26a72ced0af39b6148782d5d32c9ae37dd01f
Broken Deer <kxscluabcbook@outlook.com>
2023-07-05 16:18:59 +0800

    55511

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use tokio::process::Command;
 #[tokio::main]
 async fn main() {
     let mut command = Command::new("C:\\Windows\\System32\\cmd.exe");
-    command.args(&["/C", r#"powershell -c [System.Environment]::OSVersi"#]);
+    command.args(&["/C", r#"powershell -c [System.Environment]::OSVersion.Version"#]);
     let output = command.output().await.unwrap();
     let stdout = String::from_utf8(output.stdout).unwrap();
     let stderr = String::from_utf8(output.stderr).unwrap();
