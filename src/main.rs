5b6f867954960d3aa838952f55ecd13ee0eb6908
Broken Deer <kxscluabcbook@outlook.com>
2023-07-05 16:41:30 +0800

    123

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use regex::Regex;
 use tokio::process::Command;
 
-
 // use regex::Regex;
 // use regex::Regex;
 // use tokio::{io::AsyncBufReadExt, process::Command};
@@ -11,9 +10,8 @@ async fn main() {
     command.args(&["/C", r#"powershell -c [System.Environment]::OSVersion.Version"#]);
     let output = command.output().await.unwrap();
     let stdout = String::from_utf8(output.stdout).unwrap();
-    println!("{}", &stdout);
-    let regex = Regex::new(r"[0-9].*\S").unwrap();
-    let output = regex.find(&stdout).unwrap().as_str().to_string();
 
+    let regex = Regex::new(r"\s+").unwrap();
+    let output = regex.replace_all(&stdout, ".").to_string();
     println!("{}", output);
 }
