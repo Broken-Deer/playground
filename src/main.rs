e1755ebee2d9240d10e820e89648ec25838d2147
Broken Deer <kxscluabcbook@outlook.com>
2023-07-05 14:06:43 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,8 @@ use regex::Regex;
 use tokio::process::Command;
 #[tokio::main]
 async fn main() {
-    let  mut command = Command::new("powershell");
-    command.args(&["-Command", "chcp 65001 && ver"]);
+    let  mut command = Command::new("C:\\Windows\\System32\\cmd.exe");
+    command.args(&["/C", "chcp 65001 && ver"]);
     let output = command.output().await.unwrap();
     let stdout = String::from_utf8(output.stdout).unwrap();
     println!("{}", &stdout);
