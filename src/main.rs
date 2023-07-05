a6f68a1216c77f6d85838a28baa7fb3f0d801132
Broken Deer <kxscluabcbook@outlook.com>
2023-07-05 16:23:45 +0800

    123123

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use regex::Regex;
 use tokio::process::Command;
 
 
@@ -10,17 +11,9 @@ async fn main() {
     command.args(&["/C", r#"powershell -c [System.Environment]::OSVersion.Version"#]);
     let output = command.output().await.unwrap();
     let stdout = String::from_utf8(output.stdout).unwrap();
-    let stderr = String::from_utf8(output.stderr).unwrap();
     println!("{}", &stdout);
-    println!("{}", &stderr);
-    // let a = r"
-    // OsHardwareAbstractionLayer
-    // --------------------------
-    // 10.0.19041.2251
+    let regex = Regex::new(r"[0-9].*\S").unwrap();
+    let output = regex.find(&stdout).unwrap().as_str().to_string();
 
-    // Microsoft Windows [version 10.0]";
-    // let regex = Regex::new(r"[0-9].*\S").unwrap();
-    // let output = regex.find(&a).unwrap().as_str().to_string();
-
-    // println!("{}", output);
+    println!("{}", output);
 }
