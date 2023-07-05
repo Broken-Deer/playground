2d7e2fbbb0b1a55b116ca4b05f1f69dc3ce1393f
Broken Deer <kxscluabcbook@outlook.com>
2023-07-05 14:35:42 +0800

    1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use regex::Regex;
+// use regex::Regex;
 use tokio::process::Command;
 #[tokio::main]
 async fn main() {
@@ -7,8 +7,8 @@ async fn main() {
     let output = command.output().await.unwrap();
     let stdout = String::from_utf8(output.stdout).unwrap();
     println!("{}", &stdout);
-    let regex = Regex::new(r"\S*.\]").unwrap();
-    let mut output = regex.find(&stdout).unwrap().as_str().to_string();
-    output.pop();
-    println!("{}", output);
+    // let regex = Regex::new(r"\S*.\]").unwrap();
+    // let mut output = regex.find(&stdout).unwrap().as_str().to_string();
+    // output.pop();
+    // println!("{}", output);
 }
