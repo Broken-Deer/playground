f396e657116199244d568f1266b04c5e280d14e9
Broken Deer <kxscluabcbook@outlook.com>
2023-07-05 11:18:31 +0800

    123

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
-// #[tokio::main]
-// async fn main() {
-//     install(
-//         "1.19.4",
-//         MinecraftLocation::new("test"),
-//         EventListeners::new().on_progress(Box::new(|a, b, c| {
-//             println!("progress: {a}/{b}  step: {c}");
-//         })),
-//     )
-//     .await;
-// }
+use mgl_core::{
+    core::{folder::MinecraftLocation, task::EventListeners},
+    installer::install,
+};
 
-fn main() {
-    
+#[tokio::main]
+async fn main() {
+    install(
+        "1.19.4",
+        MinecraftLocation::new("test"),
+        EventListeners::new().on_progress(Box::new(|a, b, c| {
+            println!("progress: {a}/{b}  step: {c}");
+        })),
+    )
+    .await;
 }
